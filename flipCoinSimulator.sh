a9bfd1ce1dd68b991a3ce46e152919bf7aaecb82
Imran01000 <imshaik01000@gmail.com>
2020-02-26 18:55:18 +0530

    ADDED:Doublet combinations and their percentage respectively.

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -4,40 +4,63 @@
 #variables
 head=1;
 tail=0;
-count1=0;
-count0=0;
+number=0
+
 #Defining dictionary.
 declare -A dictionary;
-dictionary=([HEAD]=$count1 [TAIL]=$count0);
-#read -p "How many times flip do you wants?" number
-function getFlipCoin(){
-
-			flip=$(($RANDOM%2));
-			if (($flip==$head))
-			then
-				echo 1;
-			else
-				echo 0;
-			fi
-		
-}
-read -p "How many times flip do you wants?" number
-
-
+dictionary=([h]=$count1 [t]=$count0 [hh]=$count11 [tt]=$count00 [ht]=$count10 [th]=$count01);
+read -p "How many times flip do you want for singlet?" number 
 for((itr=1;itr<=$number;itr++))
 do 
-	if (("$(getFlipCoin)"==$head))
-	then
-		dictionary[HEAD]=$((${dictionary[HEAD]}+1));
-	else
-		dictionary[TAIL]=$((${dictionary[TAIL]}+1));
-	fi
+	flip=$(($RANDOM%2))
+	case $flip in
+		1)
+		dictionary[h]=$((${dictionary[h]}+1));
+		;;
+		0)
+		dictionary[t]=$((${dictionary[t]}+1));
+		;;
+	esac
 done
 for keys in ${!dictionary[@]}
 do 
 	echo "${keys} : ${dictionary[$keys]}"
 done
-headPercentage=`echo "scale=3; (${dictionary[HEAD]}*100)/$number" | bc -l`
-echo $headPercentage;
-tailPercentage=`echo "scale=3; (${dictionary[TAIL]}*100)/$number" | bc -l`
-echo $tailPercentage;
+headPercentage=`echo "scale=3; (${dictionary[h]}*100)/$number" | bc -l`
+echo "Percentage for h is : $headPercentage";
+tailPercentage=`echo "scale=3; (${dictionary[t]}*100)/$number" | bc -l`
+echo "Percentage for t is : $tailPercentage";
+	
+read -p "How many times flip do you want for doublet?" num
+	for((itr=1;itr<=$(($num*2));itr++))
+	do
+		flip=$(($RANDOM%4))
+		case $flip in
+      	0)
+   		dictionary[hh]=$((${dictionary[hh]}+1))
+			;;
+			1)
+      	dictionary[tt]=$((${dictionary[tt]}+1))
+			;;
+ 			2)  
+      	dictionary[ht]=$((${dictionary[ht]}+1))
+			;;
+			3)
+      	dictionary[th]=$((${dictionary[th]}+1))
+			;;
+   	esac
+	done
+for keys in ${!dictionary[@]}
+do 
+   echo "$keys : ${dictionary[$keys]}"
+done
+
+percentageOfhh=`echo "scale=3; (${dictionary[hh]}*100)/($num*2)" | bc -l`;
+echo "Percentage for hh is : $percentageOfhh"
+percentageOftt=`echo "scale=3; (${dictionary[tt]}*100)/($num*2)" | bc -l`;
+echo "percentage for tt is : $percentageOftt"
+percentageOfht=`echo "scale=3; (${dictionary[ht]}*100)/($num*2)" | bc -l`;
+echo "Percentage for ht is : $percentageOfht"
+percentageOfth=`echo "scale=3; (${dictionary[th]}*100)/($num*2)" | bc -l`;
+echo "Percentage for th is : $percentageOfth"
+
