907d8e2cac696eae7e874bec92f5817fc060b134
Imran01000 <imshaik01000@gmail.com>
2020-02-27 11:59:53 +0530

    REFACTORED:Made proper indentation with comments.

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -20,6 +20,7 @@ count110=0;
 count010=0;
 count100=0;
 count000=0;
+
 #Defining dictionary.
 declare -A dictionary;
 dictionary=([h]=$count1 [t]=$count0 [hh]=$count11 [tt]=$count00 [ht]=$count10 [th]=$count01 [hhh]=$count111	[thh]=$count011 [hth]=$count101 [tth]=$count001	[hht]=$count110 [tht]=$count010 [htt]=$count100 [ttt]=$count000	);
@@ -36,39 +37,47 @@ do
 		;;
 	esac
 done
+
+#Diplaying dictionary.
 for keys in ${!dictionary[@]}
 do 
 	echo "${keys} : ${dictionary[$keys]}"
 done
+
+#Calculated percentage for head and tail.
 headPercentage=`echo "scale=3; (${dictionary[h]}*100)/$number" | bc -l`
 echo "Percentage for h is : $headPercentage";
 tailPercentage=`echo "scale=3; (${dictionary[t]}*100)/$number" | bc -l`
 echo "Percentage for t is : $tailPercentage";
-	
+
+#Taking number of flip for doublet.
 read -p "How many times flip do you want for doublet?" num
 	for((itr=1;itr<=$(($num));itr++))
 	do
 		flip=$(($RANDOM%4))
 		case $flip in
-      	0)
-   		dictionary[hh]=$((${dictionary[hh]}+1))
-			;;
-			1)
-      	dictionary[tt]=$((${dictionary[tt]}+1))
-			;;
-			2)  
-      	dictionary[ht]=$((${dictionary[ht]}+1))
-			;;
-			3)
-      	dictionary[th]=$((${dictionary[th]}+1))
-			;;
-   	esac
+		0)
+		dictionary[hh]=$((${dictionary[hh]}+1))
+		;;
+		1)
+		dictionary[tt]=$((${dictionary[tt]}+1))
+		;;
+		2)  
+		dictionary[ht]=$((${dictionary[ht]}+1))
+		;;
+		3)
+		dictionary[th]=$((${dictionary[th]}+1))
+		;;
+		esac
 	done
+
+#Displaying dictionary.
 for keys in ${!dictionary[@]}
 do 
    echo "$keys : ${dictionary[$keys]}"
 done
 
+#Calculated percentage for head and tail combinations.
 percentageOfhh=`echo "scale=3; (${dictionary[hh]}*100)/$num" | bc -l`;
 echo "Percentage for hh is : $percentageOfhh"
 percentageOftt=`echo "scale=3; (${dictionary[tt]}*100)/$num" | bc -l`;
@@ -78,6 +87,7 @@ echo "Percentage for ht is : $percentageOfht"
 percentageOfth=`echo "scale=3; (${dictionary[th]}*100)/$num" | bc -l`;
 echo "Percentage for th is : $percentageOfth"
 
+#taking number of flip for triplet.
 read -p "How many times flip do you want for triplet?" num
    for((itr=1;itr<=$(($num));itr++))
    do
@@ -110,11 +120,14 @@ read -p "How many times flip do you want for triplet?" num
 
       esac
    done
+
+#Displaying dictionary.
 for keys in ${!dictionary[@]}
 do 
    echo "$keys : ${dictionary[$keys]}"
 done
 
+#calculated percentage for head and tail combination.
 percentageOfhhh=`echo "scale=3; (${dictionary[hhh]}*100)/$num" | bc -l`;
 echo "Percentage for hhh is : $percentageOfhhh"
 percentageOfthh=`echo "scale=3; (${dictionary[thh]}*100)/$num" | bc -l`;
@@ -132,20 +145,19 @@ echo "Percentage for htt is : $percentageOfhtt"
 percentageOfttt=`echo "scale=3; (${dictionary[ttt]}*100)/$num" | bc -l`;
 echo "Percentage for ttt is : $percentageOfttt"
 
-for itr in ${dictionary[@]}
-do 
-	flipArray[((count++))]=$itr;
-done
-for((itr=0;itr<=13;itr++))
-do 
-	for((inr=0;inr<=12;inr++))
-	do
-		if (("${flipArray[$inr]}" > "${flipArray[$(($inr+1))]}"))
-		then
-				temp="${flipArray[$inr]}";
-				flipArray[inr]="${flipArray[inr+1]}";
-				flipArray[inr+1]=$temp;
-		fi
-	done
-done
-echo ${flipArray[@]}
+#Sorted the dictionary
+echo "Sorted values"
+for keys in ${!dictionary[@]}
+do
+	echo "$keys : ${dictionary[$keys]}"
+done | sort -k3
+
+#Displaying Win combination.
+echo "Wining combination"
+for keys in ${!dictionary[@]}
+do
+   echo "$keys : ${dictionary[$keys]}"
+done | sort -k3 | tail -1; 
+
+
+
